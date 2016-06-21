1940ca597e2e03331e993dbb250a5e19b809e284
Anton Gulenko <anton.gulenko@googlemail.com>
2016-06-21 16:25:15 +0200

    Prototype DecisionTree Analyser: Removed capability of Analyse class to train the model on demand. Trained model is now required.

--- src/main/java/metrics/main/prototype/Analyse.java
+++ src/main/java/metrics/main/prototype/Analyse.java
@@ -1,14 +1,10 @@
 package metrics.main.prototype;
 
-import metrics.algorithms.AbstractFeatureScaler;
 import metrics.algorithms.FeatureAggregator;
-import metrics.algorithms.FeatureStandardizer;
 import metrics.algorithms.OnlineFeatureStandardizer;
 import metrics.algorithms.classification.Model;
-import metrics.algorithms.classification.WekaLearner;
 import metrics.algorithms.classification.WekaOnlineClassifier;
 import metrics.io.MetricPrinter;
-import metrics.io.file.FileMetricReader;
 import metrics.io.fork.TwoWayFork;
 import metrics.io.net.TcpMetricsOutput;
 import metrics.main.AlgorithmPipeline;
@@ -16,10 +12,11 @@ import metrics.main.TrainedDataModel;
 import metrics.main.analysis.OpenStackSampleSplitter;
 import weka.classifiers.trees.J48;
 
-import java.io.*;
-import java.util.HashMap;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.HashSet;
-import java.util.Map;
 
 /**
  * Created by anton on 6/9/16.
@@ -27,12 +24,11 @@ import java.util.Map;
 public class Analyse {
 
     static final String TCP_FORMAT = "BIN";
-    static final String TRAINING_FORMAT = "BIN";
     static final String TCP_OUTPUT_FORMAT = "BIN";
 
     public static void main(String[] args) throws IOException {
         if (args.length != 5) {
-            System.err.println("Parameters: <receive-port> <" + TRAINING_FORMAT + " file> <target-host> <target-port> <local hostname>");
+            System.err.println("Parameters: <receive-port> <trained model file> <target-host> <target-port> <local hostname>");
             return;
         }
         int receivePort = Integer.parseInt(args[0]);
@@ -65,57 +61,13 @@ public class Analyse {
     }
 
     static TrainedDataModel getDataModel(String inputFile) throws IOException {
-        String cacheFile = inputFile + ".model";
-        TrainedDataModel dataModel = loadDataModel(cacheFile);
-        if (dataModel != null) {
-            System.err.println("Loaded cached model from " + cacheFile);
-            return dataModel;
-        }
-        System.err.println("No cached model, computing model...");
-        dataModel = createDataModel(inputFile);
-        storeDataModel(cacheFile, dataModel);
-        return dataModel;
-    }
-
-    static TrainedDataModel createDataModel(String inputFile) throws IOException {
-        J48 j48 = new J48();
-        FeatureStandardizer standardizer = new FeatureStandardizer();
-        WekaLearner<J48> learner = new WekaLearner<>(new Model<>(), j48);
-
-        new AlgorithmPipeline(AlgorithmPipeline.fileReader(inputFile, TRAINING_FORMAT, FileMetricReader.FILE_NAME))
-                .fork(new OpenStackSampleSplitter(),
-                        (name, p) -> {
-                            if (!name.isEmpty()) {
-                                System.err.println("Error: received hostname from OpenstackSampleSplitter: " + name);
-                                return;
-                            }
-
-                            p
-                                    .step(new FeatureAggregator(10000L).addAvg().addSlope())
-                                    .step(standardizer)
-                                    .step(learner);
-                        })
-                .runAndWait();
-
-        TrainedDataModel dataModel = new TrainedDataModel();
-        dataModel.model = j48;
-        dataModel.averages = new HashMap<>();
-        dataModel.stddevs = new HashMap<>();
-        dataModel.allClasses = learner.allFlushedClasses;
-        dataModel.headerFields = learner.flushedHeader.header;
-        for (Map.Entry<String, AbstractFeatureScaler.MetricScaler> entry : standardizer.getScalers().entrySet()) {
-            String name = entry.getKey();
-            AbstractFeatureScaler.MetricScaler scaler = entry.getValue();
-            if (!(scaler instanceof FeatureStandardizer.MetricStandardizer)) {
-                throw new IllegalStateException("MetricScaler was not FeatureStandardizer.MetricStandardizer, but " +
-                        scaler.getClass().toString());
-            }
-            FeatureStandardizer.MetricStandardizer metric = (FeatureStandardizer.MetricStandardizer) scaler;
-            dataModel.averages.put(name, metric.average);
-            dataModel.stddevs.put(name, metric.stdDeviation);
+        TrainedDataModel dataModel = loadDataModel(inputFile);
+        if (dataModel == null) {
+            throw new IOException("Failed to load trained model from " + inputFile);
         }
         return dataModel;
     }
+
 
     static TrainedDataModel loadDataModel(String cacheFile) throws IOException {
         if (!new File(cacheFile).exists()) return null;
@@ -134,14 +86,6 @@ public class Analyse {
             throw new IOException("Object in file " + cacheFile + " was " +
                     obj.getClass().toString() + " instead of DataModel");
         }
-    }
-
-    static void storeDataModel(String cacheFile, TrainedDataModel model) throws IOException {
-        System.err.println("Storing model to " + cacheFile);
-        FileOutputStream file_out = new FileOutputStream(cacheFile);
-        ObjectOutputStream obj_out = new ObjectOutputStream(file_out);
-        obj_out.writeObject(model);
-        obj_out.close();
     }
 
 }
--- src/main/java/metrics/main/prototype/Train.java
+++ src/main/java/metrics/main/prototype/Train.java
@@ -1,8 +1,21 @@
 package metrics.main.prototype;
 
+import metrics.algorithms.AbstractFeatureScaler;
+import metrics.algorithms.FeatureAggregator;
+import metrics.algorithms.FeatureStandardizer;
+import metrics.algorithms.classification.Model;
+import metrics.algorithms.classification.WekaLearner;
+import metrics.io.file.FileMetricReader;
+import metrics.main.AlgorithmPipeline;
 import metrics.main.TrainedDataModel;
+import metrics.main.analysis.OpenStackSampleSplitter;
+import weka.classifiers.trees.J48;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by anton on 6/9/16.
@@ -10,14 +23,64 @@ import java.io.IOException;
 public class Train {
 
     static final String TRAINING_INPUT_FORMAT = "BIN";
+    static final String TRAINING_FORMAT = "BIN";
 
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             System.err.println("Need 2 parameters: <input " + TRAINING_INPUT_FORMAT + " file> <output file>");
             return;
         }
-        TrainedDataModel model = Analyse.createDataModel(args[0]);
-        Analyse.storeDataModel(args[1], model);
+        TrainedDataModel model = createDataModel(args[0]);
+        storeDataModel(args[1], model);
     }
+
+    static TrainedDataModel createDataModel(String inputFile) throws IOException {
+        J48 j48 = new J48();
+        FeatureStandardizer standardizer = new FeatureStandardizer();
+        WekaLearner<J48> learner = new WekaLearner<>(new Model<>(), j48);
+
+        new AlgorithmPipeline(AlgorithmPipeline.fileReader(inputFile, TRAINING_FORMAT, FileMetricReader.FILE_NAME))
+                .fork(new OpenStackSampleSplitter(),
+                        (name, p) -> {
+                            if (!name.isEmpty()) {
+                                System.err.println("Error: received hostname from OpenstackSampleSplitter: " + name);
+                                return;
+                            }
+
+                            p
+                                    .step(new FeatureAggregator(10000L).addAvg().addSlope())
+                                    .step(standardizer)
+                                    .step(learner);
+                        })
+                .runAndWait();
+
+        TrainedDataModel dataModel = new TrainedDataModel();
+        dataModel.model = j48;
+        dataModel.averages = new HashMap<>();
+        dataModel.stddevs = new HashMap<>();
+        dataModel.allClasses = learner.allFlushedClasses;
+        dataModel.headerFields = learner.flushedHeader.header;
+        for (Map.Entry<String, AbstractFeatureScaler.MetricScaler> entry : standardizer.getScalers().entrySet()) {
+            String name = entry.getKey();
+            AbstractFeatureScaler.MetricScaler scaler = entry.getValue();
+            if (!(scaler instanceof FeatureStandardizer.MetricStandardizer)) {
+                throw new IllegalStateException("MetricScaler was not FeatureStandardizer.MetricStandardizer, but " +
+                        scaler.getClass().toString());
+            }
+            FeatureStandardizer.MetricStandardizer metric = (FeatureStandardizer.MetricStandardizer) scaler;
+            dataModel.averages.put(name, metric.average);
+            dataModel.stddevs.put(name, metric.stdDeviation);
+        }
+        return dataModel;
+    }
+
+    static void storeDataModel(String cacheFile, TrainedDataModel model) throws IOException {
+        System.err.println("Storing model to " + cacheFile);
+        FileOutputStream file_out = new FileOutputStream(cacheFile);
+        ObjectOutputStream obj_out = new ObjectOutputStream(file_out);
+        obj_out.writeObject(model);
+        obj_out.close();
+    }
+
 
 }
