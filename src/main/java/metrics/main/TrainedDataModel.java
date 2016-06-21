a41b27d1b6ff3136a6471aedaf099b3646328931
Anton Gulenko <anton.gulenko@googlemail.com>
2016-06-21 16:05:03 +0200

    added note

--- src/main/java/metrics/main/TrainedDataModel.java
+++ src/main/java/metrics/main/TrainedDataModel.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.Map;
 
 /**
+ * IMPORTANT: This class must stay in this package to make use of previously serialized instances.
+ *
  * Created by anton on 6/14/16.
  */
 public class TrainedDataModel implements Serializable {
