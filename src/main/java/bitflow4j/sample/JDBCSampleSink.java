6ab288af612fbd4e86a1ead6964609c629fb61c4
Marvin Byfield <marvin.cm.byfield@campus.tu-berlin.de>
2017-02-15 14:41:35 +0100

    Initial commit for JDBC Sample Source and Sink

---
+++ src/main/java/bitflow4j/sample/JDBCSampleSink.java
@@ -0,0 +1,13 @@
+package bitflow4j.sample;
+
+import java.io.IOException;
+
+/**
+ * Created by malcolmx on 15.02.17.
+ */
+public class JDBCSampleSink extends AbstractSampleSink {
+    @Override
+    public void writeSample(Sample sample) throws IOException {
+
+    }
+}
