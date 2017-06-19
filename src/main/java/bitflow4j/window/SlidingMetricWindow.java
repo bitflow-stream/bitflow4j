e01d93d7596ead9f52b3843480c47248a50a2d5f
Alexander Acker <alexander.acker@tu-berlin.de>
2017-06-19 18:37:42 +0200

    Rework of sliding metric window

--- src/main/java/bitflow4j/window/SlidingMetricWindow.java
+++ src/main/java/bitflow4j/window/SlidingMetricWindow.java
@@ -1,6 +1,6 @@
 package bitflow4j.window;
 
-import java.util.LinkedList;
+import gnu.trove.list.linked.TDoubleLinkedList;
 
 /**
  * @author fschmidt, Alex (15.05.2017)
@@ -10,28 +10,24 @@ public class SlidingMetricWindow extends AbstractMetricWindow {
     //TODO: Add header changed case
     //TODO: add timeSpan as parameter
     private final int windowSize;
-    private final LinkedList<Double> window;
+    private final TDoubleLinkedList window;
 
     public SlidingMetricWindow(String name, int windowSize) {
         super(name);
         this.windowSize = windowSize;
-        this.window = new LinkedList<>();
+        this.window = new TDoubleLinkedList();
     }
 
     @Override
     public double[] getVector() {
-        double[] returnVector = new double[window.size()];
-        for (int i = 0; i < window.size(); i++) {
-            returnVector[i] = window.get(i);
-        }
-        return returnVector;
+        return window.toArray();
     }
 
     @Override
     public void add(double val) {
-        window.offer(val);
+        window.add(val);
         if (windowSize > 0 && window.size() > windowSize) {
-            window.poll();
+            window.removeAt(0);
         }
     }
 
