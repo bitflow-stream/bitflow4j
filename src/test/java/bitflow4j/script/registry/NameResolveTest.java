d4d7b91f89b177ee6aca63440b4129705205368d
Kevin Styp <kevinstyp@dhcp-248-239.cit.tu-berlin.de>
2019-04-17 11:30:41 +0200

    Added multiple tests to test the resolving of classnames according to the new naming convention.

---
+++ src/test/java/bitflow4j/script/registry/NameResolveTest.java
@@ -0,0 +1,58 @@
+package bitflow4j.script.registry;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NameResolveTest {
+
+    private AbstractRegisteredStep constructRegisteredStep(String name) {
+        AbstractRegisteredStep testObject = new AbstractRegisteredStep(name, "") {
+            @Override
+            public AbstractRegisteredStep optional(String... parameters) {
+                return super.optional(parameters);
+            }
+        };
+        return testObject;
+    }
+
+    private void testExpectedResult(String input, String expectation) {
+        AbstractRegisteredStep testObject = constructRegisteredStep(input);
+        assertEquals(expectation, testObject.getStepName());
+    }
+
+    @Test
+    public void testNameResolveSimpleCases() {
+        testExpectedResult("Name", "name");
+        testExpectedResult("SimpleName", "simple-name");
+        testExpectedResult("Superlongestclassnameieversaw", "superlongestclassnameieversaw");
+        testExpectedResult("N", "n");
+        testExpectedResult("ALLBIG", "allbig");
+    }
+
+    @Test
+    public void testNameResolveEdgeCases() {
+        testExpectedResult("", "");
+        testExpectedResult("KMeans", "k-means");
+        testExpectedResult("KAMeans", "ka-means");
+        testExpectedResult("SomeKMeans", "some-k-means");
+        testExpectedResult("SomeKAMeans", "some-ka-means");
+        testExpectedResult("SomeMeansK", "some-means");
+        testExpectedResult("SomeMeansKA", "some-means-ka");
+        testExpectedResult("FFTCalculator", "fft-calculator");
+        testExpectedResult("CalculateFFT", "calculate-fft");
+        testExpectedResult("BIGSmallBIGSmall", "big-small-big-small");
+    }
+
+    @Test
+    public void testNameResolveStepStripping() {
+        testExpectedResult("MyFancyStep", "my-fancy");
+        testExpectedResult("MyFancyProcessingStep", "my-fancy");
+        testExpectedResult("MyFancyBatchStep", "my-fancy");
+        testExpectedResult("StepWiseCalculator", "step-wise-calculator");
+        testExpectedResult("calculateBatchStepMinMax", "calculate-batch-step-min-max");
+        testExpectedResult("BigPROCESSINGSTEP", "big");
+        testExpectedResult("AStep", "a");
+    }
+
+}
