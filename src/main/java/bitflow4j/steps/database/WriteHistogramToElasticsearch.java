fef3d2aa4ad4dfce9353a06e57f1691768853df7
Kevin Styp <styp-rekowski@tu-berlin.de>
2019-05-21 15:06:15 +0200

    Rework of Elasticsearch DB step: Code formatting, parameter parsing, logging, documentation.

--- src/main/java/bitflow4j/steps/database/WriteHistogramToElasticsearch.java
+++ src/main/java/bitflow4j/steps/database/WriteHistogramToElasticsearch.java
@@ -1,6 +1,7 @@
 package bitflow4j.steps.database;
 
 import bitflow4j.Sample;
+import bitflow4j.misc.Pair;
 import bitflow4j.steps.BatchHandler;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.DocWriteResponse;
@@ -14,36 +15,63 @@ import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class WriteHistogramToElasticsearch implements BatchHandler {
 
     protected static final Logger logger = Logger.getLogger(WriteHistogramToElasticsearch.class.getName());
 
+    private final String hostports;
     private final String indexName;
     private final String identifierKey;
     private final String identifierTemplate;
     private final RestHighLevelClient client;
-    public WriteHistogramToElasticsearch(String host, int port1, int port2, String indexName, String identifierKey, String identifierTemplate) {
+
+    /**
+     * @param hostports          Comma-separated list of host:port pairs, e.g.: host1:port1, host2:port2
+     * @param indexName          Index name of the respective index in the Elasticsearch-Database
+     * @param identifierKey      Name of key-tag which is saved for each sample and used to identify/filter the entry in
+     *                           the database (the same name will be applied in the DB entry as the property name)
+     * @param identifierTemplate Used template to fill the named property with meaningful content (Tag-templates
+     *                           should be used here)
+     */
+    public WriteHistogramToElasticsearch(String hostports, String indexName, String identifierKey, String identifierTemplate) {
+        this.hostports = hostports;
         this.indexName = indexName;
         this.identifierKey = identifierKey;
         this.identifierTemplate = identifierTemplate;
+
+        List<Pair<String, Integer>> hostPortPairs = convertHostPortArgs(hostports);
+        HttpHost[] httpHosts = new HttpHost[hostPortPairs.size()];
+        for (int i = 0; i < hostPortPairs.size(); i++) {
+            Pair<String, Integer> hostport = hostPortPairs.get(i);
+            httpHosts[i] = new HttpHost(hostport.getLeft(), hostport.getRight(), "http");
+        }
         this.client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost(host, port1, "http"),
-                        new HttpHost(host, port2, "http")));
+                RestClient.builder(httpHosts));
+    }
+
+    private static List<Pair<String, Integer>> convertHostPortArgs(String tags) {
+        return Arrays.stream(tags.split(",")).map(String::trim)
+                .map(s -> {
+                    String[] hostports = s.split(":");
+                    return new Pair<String, Integer>(hostports[0], Integer.valueOf(hostports[1]));
+                }).collect(Collectors.toList());
     }
 
     @Override
     public List<Sample> handleBatch(List<Sample> batch) throws IOException {
 
+        // 'Index' in IndexRequest stands for Putting data into the DB
         IndexRequest request = new IndexRequest(indexName);
+
         request.id();
-        for (int i = 0; i < batch.size(); i++) {
-            // For each sample of the batch
-            Sample sample = batch.get(i);
+        // For each sample of the batch
+        for (Sample sample : batch) {
             double[] metrics = sample.getMetrics();
 
             //Generate the data point which represents one frequency with its amplitudes (all have the same timestamp)
@@ -57,33 +85,39 @@ public class WriteHistogramToElasticsearch implements BatchHandler {
             }
             data.endObject();
 
-            //Index stands for Putting data
             request.source(data);
 
             IndexResponse response = client.index(request, RequestOptions.DEFAULT);
-            printResponse(response);
+            printResponse(response, sample);
         }
-
         return batch;
     }
-    private void printResponse(IndexResponse indexResponse){
+
+    private void printResponse(IndexResponse indexResponse, Sample sample) {
         String index = indexResponse.getIndex();
         String id = indexResponse.getId();
         if (indexResponse.getResult() == DocWriteResponse.Result.CREATED) {
             // Object was created in Database
+            logger.log(Level.FINE, "Document was created.");
         } else if (indexResponse.getResult() == DocWriteResponse.Result.UPDATED) {
-            logger.log(Level.INFO, "Rewritten existing document");
+            logger.log(Level.FINE, String.format("Rewritten existing document: %s", toString(sample)));
         }
         ReplicationResponse.ShardInfo shardInfo = indexResponse.getShardInfo();
         if (shardInfo.getTotal() != shardInfo.getSuccessful()) {
-            // Something with shards.
+            logger.log(Level.WARNING, String.format("Total-Shards (%s) did not match successful Shards (%s): %s",
+                    shardInfo.getTotal(), shardInfo.getSuccessful(), toString(sample)));
         }
         if (shardInfo.getFailed() > 0) {
             for (ReplicationResponse.ShardInfo.Failure failure :
                     shardInfo.getFailures()) {
                 String reason = failure.reason();
-                logger.log(Level.SEVERE, String.format("Failure: %s ", failure.reason()));
+                logger.log(Level.SEVERE, String.format("Failure (%s):\n %s ", toString(sample), failure.reason()));
             }
         }
+    }
+
+    public String toString(Sample sample) {
+        return String.format("WriteHistogramToElasticSearch: Hostports: %s \nIndex: %s, Identifier = Identifier-Value: %s = %s",
+                hostports, indexName, identifierKey, sample.resolveTagTemplate(identifierTemplate));
     }
 }
